8651ef45d41ad8a2f645c89576e6b28beecbad80
Vitaliy Ostrovsky <vitaliy.ostrovsky@gmail.com>
2026-09-11 08:37:42 +0300

    [benchmarks] cpp-gtest-fix: pin source repo for stability

--- benchmarks/cpp-gtest-fix/setup.sh
+++ benchmarks/cpp-gtest-fix/setup.sh
@@ -5,6 +5,7 @@ set -e
 TARGET_DIR="./workspace/googletest"
 REPO_URL="https://github.com/google/googletest.git"
 LOCAL_FALLBACK="../../targets/cpp-gtest"
+COMMIT_SHA="283c17563fe7a1111cd7f581aa5d541e8baeff2f" # просто один из нормальных не старых комитов
 
 mkdir -p ./workspace
 
